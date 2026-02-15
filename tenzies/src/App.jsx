0e69a3127ec35756bd30837fb3a80c0a7cbddb11
JoshuaT88 <jatiller88@gmail.com>
2026-02-15 02:31:13 -0500

    //finished up the base functionality of generating //a random number for each dice. //added a 'Roll' button that calls for a re-render //and re-generation of 10 more random numbers //to attach to each dice. //Next I will convert this to an array of objects //to allow the dice to be clicked on to prevent/hold //the current value of the dice.

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,51 +1,33 @@
-import { useState } from 'react'
+
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Die from './components/Die'
 import './App.css'
 
 function App() {
-  
-function generateAllNewDice(count = 10) {
-  
-  return Array.from({ length: count }, () => Math.floor(Math.random() * 6) + 1)
-}
-console.log(generateAllNewDice())
+  const [diceArray, setDiceArray] = React.useState(generateAllNewDice());
+
+ function generateAllNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            const rand = Math.ceil(Math.random() * 6)
+            newDice.push(rand)
+        }
+        return newDice
+    }
+   const diceElements = diceArray.map(num => <Die value={num} />)
+
+   function diceRoll(){
+    setDiceArray(generateAllNewDice())
+   }
   return (
     <>
       <main>
         <div className="diceContainer">
-        <Die 
-          value='1'
-        />
-        <Die 
-          value='2'
-        />
-        <Die 
-          value='3'
-        />
-        <Die 
-          value='4'
-        />
-        <Die 
-          value='5'
-        />
-        <Die 
-          value='6'
-        />
-        <Die 
-          value='1'
-        />
-        <Die 
-          value='2'
-        />
-        <Die 
-          value='3'
-        />
-        <Die 
-          value='4'
-        />
+          {diceElements}
         </div>
+        <button className="diceRoll" onClick={diceRoll}>Roll</button>
       </main>
     </>
   )

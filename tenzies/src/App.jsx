2622e6295fe771fa0f0b3d43015eaa3e893d26d4
JoshuaT88 <jatiller88@gmail.com>
2026-02-15 01:48:32 -0500

    //Created Die.jsx to hold the Dice buttons. //State/props & math live in App.jsx //So that Die.jsx can be effected dynamically. //CSS added for clean layout. //Made random number generator for the Dice and //have it console logging for now for testing.

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,15 +1,51 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Die from './components/Die'
 import './App.css'
 
 function App() {
   
-
+function generateAllNewDice(count = 10) {
+  
+  return Array.from({ length: count }, () => Math.floor(Math.random() * 6) + 1)
+}
+console.log(generateAllNewDice())
   return (
     <>
       <main>
-        
+        <div className="diceContainer">
+        <Die 
+          value='1'
+        />
+        <Die 
+          value='2'
+        />
+        <Die 
+          value='3'
+        />
+        <Die 
+          value='4'
+        />
+        <Die 
+          value='5'
+        />
+        <Die 
+          value='6'
+        />
+        <Die 
+          value='1'
+        />
+        <Die 
+          value='2'
+        />
+        <Die 
+          value='3'
+        />
+        <Die 
+          value='4'
+        />
+        </div>
       </main>
     </>
   )

329d72c5f4a8beeb31bb5466b365a4bcbfe11923
JoshuaT88 <jatiller88@gmail.com>
2026-02-18 00:11:17 -0500

    //Installed Nanoid for the key property. //updated the generate function to convert the state set array //to be an object so that I can setup the functionality //to allow a click controlled hold function. //I finally updated the CSS to be a little cleaner then before.

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,36 +1,34 @@
 
 import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Die from './components/Die'
 import './App.css'
+import { nanoid } from 'nanoid'
 
-function App() {
-  const [diceArray, setDiceArray] = React.useState(generateAllNewDice());
+export default function App() {
+    const [dice, setDice] = React.useState(generateAllNewDice())
 
- function generateAllNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            const rand = Math.ceil(Math.random() * 6)
-            newDice.push(rand)
-        }
-        return newDice
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
     }
-   const diceElements = diceArray.map(num => <Die value={num} />)
 
-   function diceRoll(){
-    setDiceArray(generateAllNewDice())
-   }
-  return (
-    <>
-      <main>
-        <div className="diceContainer">
-          {diceElements}
-        </div>
-        <button className="diceRoll" onClick={diceRoll}>Roll</button>
-      </main>
-    </>
-  )
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
+
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
-
-export default App
